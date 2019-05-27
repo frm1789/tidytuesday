fd60547b71567220e69e52245f76664818e577f7
frm1789 <florencia.mangini@gmail.com>
2019-05-26 23:07:22 -0500

    Update 2019_21_Plastic_waste.R

--- 2019_21_plastic_waste/2019_21_Plastic_waste.R
+++ 2019_21_plastic_waste/2019_21_Plastic_waste.R
@@ -9,38 +9,35 @@ library(viridis)
 library(scales)
 
 #Data
-coast_vs_waste <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-21/coastal-population-vs-mismanaged-plastic.csv")
+coast_vs_waste <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-21/coastal-population-vs-mismanaged-plastic.csv") 
 mismanaged_vs_gdp <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-21/per-capita-mismanaged-plastic-waste-vs-gdp-per-capita.csv")
 waste_vs_gdp <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-21/per-capita-plastic-waste-vs-gdp-per-capita.csv")
 
-#Colors
-q_colors =  5 # for continents
-v_colors =  viridis(q_colors, option = "D")
+#clean names 
+colnames(coast_vs_waste) <- c("entity", "code", "year", "total_mismanaged_plastic","coastal_population", "population")
+colnames(mismanaged_vs_gdp) <- c("entity", "code", "year", "per_capita_mismanaged_plastic","per_capita_gdp", "population")
+colnames(waste_vs_gdp) <- c("entity", "code", "year", "per_capita_plastic_waste","per_capita_gdp", "population")
 
-#Cleaning
+#clean na
 df_waste <- waste_vs_gdp %>%
-  setNames(c("entity", "code", "year", "per_capita_plastic_waste",
-             "per_capita_gdp", "population")) %>%
   drop_na(per_capita_plastic_waste, per_capita_gdp, population)
 
 df_mismanaged <- mismanaged_vs_gdp %>%
-  setNames(c("entity", "code", "year", "per_capita_mismanaged_plastic",
-             "per_capita_gdp", "population")) %>%
   drop_na(per_capita_mismanaged_plastic, per_capita_gdp, population)
 
 df_coast <- coast_vs_waste %>%
-  setNames(c("entity", "code", "year", "total_mismanaged_plastic",
-             "coastal_population", "population")) %>%
   drop_na(total_mismanaged_plastic, coastal_population, population)
+
 
 #Create a unique df
 df_waste_misma <- df_waste %>%
   full_join(df_mismanaged, by = c("entity", "code", "year", "per_capita_gdp", "population")) %>%
+  full_join(df_coast, by = c("entity", "code", "year", "population"))%>%
   mutate(continent = countrycode(sourcevar = entity, 
                                  origin = "country.name.en", 
                                  destination = "continent")) %>%
-  full_join(df_coast, by = c("entity", "code", "year", "population"))%>%
   drop_na(per_capita_plastic_waste, per_capita_gdp)
+
 
 #ggcorr looking for correlations
 df_numbers <- df_waste_misma %>%
@@ -53,13 +50,16 @@ ggpairs(df_numbers, cardinality_threshold = 148) +
        caption = "Source: Our World in Data | by: Florencia Mangini") +
   theme_minimal() 
 
+#Colors
+q_colors =  5 # for continents
+v_colors =  viridis(q_colors, option = "D")
 
 
 ## 1) per_capita_mismanaged_plastic vs. coastal population: 0.783
 top_vis1 <- df_waste_misma[df_waste_misma$coastal_population > 20000949 & 
-                        df_waste_misma$coastal_population <= max(df_waste_misma$coastal_population) & 
-                        df_waste_misma$total_mismanaged_plastic > 15466 & 
-                        df_waste_misma$total_mismanaged_plastic <= max(df_waste_misma$total_mismanaged_plastic), ]
+                             df_waste_misma$coastal_population <= max(df_waste_misma$coastal_population) & 
+                             df_waste_misma$total_mismanaged_plastic > 15466 & 
+                             df_waste_misma$total_mismanaged_plastic <= max(df_waste_misma$total_mismanaged_plastic), ]
 
 
 vis1 <- ggplot(df_waste_misma, aes(y=total_mismanaged_plastic, x=coastal_population)) + 
@@ -77,7 +77,6 @@ plot(vis1)
 
 
 ## 2) per_capita_mismanaged_plastic vs. per_capita_gdp: -0.43
-
 vis2 <- ggplot(df_waste_misma, aes(x=per_capita_mismanaged_plastic, y=per_capita_gdp)) + 
   geom_point(aes(col=continent, size=per_capita_mismanaged_plastic)) + 
   scale_color_manual(values=c("#440154FF", "#3B528BFF", "#21908CFF", "#5DC863FF", "#FDE725FF"))+
@@ -90,8 +89,6 @@ vis2 <- ggplot(df_waste_misma, aes(x=per_capita_mismanaged_plastic, y=per_capita
 plot(vis2)
 
 
-
-
 ## 3) Mismanaged plastic vs. plastic waste: 0.27
 ##Ratio bewteen waste_ratio & mismanaged_plastic
 # per_capita_mismanaged_plastic: Amount of mismanaged plastic waste per capita in kg/day
@@ -100,9 +97,9 @@ df_waste_misma <- df_waste_misma %>%
   mutate(ratio_waste = per_capita_mismanaged_plastic / per_capita_plastic_waste) 
 
 top_vis3 <- df_waste_misma[df_waste_misma$ratio_waste > 0.50 & 
-                df_waste_misma$ratio_waste <= 0.99 & 
-                df_waste_misma$per_capita_mismanaged_plastic > 0.135 & 
-                df_waste_misma$per_capita_mismanaged_plastic < 0.4, ]
+                             df_waste_misma$ratio_waste <= 0.99 & 
+                             df_waste_misma$per_capita_mismanaged_plastic > 0.135 & 
+                             df_waste_misma$per_capita_mismanaged_plastic < 0.4, ]
 
 vis3 <- ggplot(df_waste_misma, aes(x=ratio_waste, y=per_capita_mismanaged_plastic)) + 
   geom_point(aes(col=continent, size=per_capita_mismanaged_plastic )) + 
@@ -122,6 +119,3 @@ vis3 <- ggplot(df_waste_misma, aes(x=ratio_waste, y=per_capita_mismanaged_plasti
        caption = "Source: Our World in Data | by: Florencia Mangini")+
   theme_minimal() 
 plot(vis3)
-
-
-
