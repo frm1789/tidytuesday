ac34f9a93dd708f7430cc187fccbb72b63b450a2
frm1789 <florencia.mangini@gmail.com>
2019-10-24 10:52:33 -0500

    Update code.R

--- 2019_43_Horror_Movies/code.R
+++ 2019_43_Horror_Movies/code.R
@@ -1,8 +1,14 @@
 horror_movies <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-10-22/horror_movies.csv")
 
+# About Money and Rating
+
 # 1) Is it a good business to produce horror movies?
-
 # 2) In terms of profit, is horror the best genre in the industry?
-
 # 3) Is relevant the cast in the total rating or in profit? 
 
+
+
+# About Hallowen
+
+# 1) If I release a movie on Hallowen, I will obtain better profit, compared with other with similar budget?
+# 2) In others countries, not USA and Canada, wich is the month when more productions are launched?
