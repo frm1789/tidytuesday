0a9603cb0b87437f4142ae66a998c0ac6904111f
frm1789 <florencia.mangini@gmail.com>
2019-10-24 10:43:08 -0500

    Adding code

--- 2019_43_Horror_Movies/code.R
+++ 2019_43_Horror_Movies/code.R
@@ -1,2 +1,8 @@
 horror_movies <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-10-22/horror_movies.csv")
 
+# 1) Is it a good business to produce horror movies?
+
+# 2) In terms of profit, is horror the best genre in the industry?
+
+# 3) Is relevant the cast in the total rating or in profit? 
+
