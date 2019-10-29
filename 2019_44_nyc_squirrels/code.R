8080ea24e38d0abfa67ff6ee1913077e7c1193a9
frm1789 <florencia.mangini@gmail.com>
2019-10-28 22:49:51 -0500

    Update code.R

--- 2019_44_nyc_squirrels/code.R
+++ 2019_44_nyc_squirrels/code.R
@@ -5,3 +5,22 @@ tidy_sq <- nyc_squirrels
 # Little exploration
 skimr::skim(nyc_squirrels)
 head(nyc_squirrels)
+
+# hectare_squirrel_number
+# hectare # 37F # 05D
+tidy_sq <- transform(tidy_sq, hectare_v = substr(hectare, 1, 2), hectare_h = substr(hectare, 3,3))
+
+tidy_sq_max <- tidy_sq %>%
+  select(hectare_h, hectare_v, shift, date, hectare_squirrel_number) %>%
+  group_by(hectare_h, hectare_v, shift, date) %>%
+  summarise(max_value = max(hectare_squirrel_number)) %>%
+  arrange(desc(date))
+
+tidy_sq_max_total <- tidy_sq_max %>%
+  group_by(hectare_h, hectare_v, shift, date) %>%
+  summarise(total = sum(max_value)) %>% 
+  arrange(desc(date))
+
+
+# Heatmap day and night
+
