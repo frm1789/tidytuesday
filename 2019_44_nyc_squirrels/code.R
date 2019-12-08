2ba172c14390a7fa94ef5c5cd75b7a09ef15a633
Florencia Mangini <florencia.mangini@gmail.com>
2019-12-07 23:40:02 -0600

    Update code.R

--- 2019_44_nyc_squirrels/code.R
+++ 2019_44_nyc_squirrels/code.R
@@ -5,8 +5,3 @@ library(reshape)
 nyc_squirrels <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-10-29/nyc_squirrels.csv")
 tidy_sq <- nyc_squirrels
 
-#tidy dataframe
-tidy_sq <- transform(tidy_sq, hectare_v = substr(hectare, 1, 2), hectare_h = substr(hectare, 3,3))
-
-
-
